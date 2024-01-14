c13c5002ef2f22ae5c1847c0c417866eab9ec9e0
Elena Lyzo <elena.lyzo.69@gmail.com>
2024-01-14 23:09:31 +0300

    адача4

--- Lesson2/Task4/Program.cs
+++ Lesson2/Task4/Program.cs
@@ -1,7 +1,7 @@
 ﻿int n = 5;
-int[] array = {2,4,7,9,12};
+int[] array = { 2, 4, 7, 9, 12 };
 int i = 0;
-int max = array [0];
+int max = array[0];
 
 while (i < n)
 {
@@ -10,5 +10,25 @@ while (i < n)
         max = array[i];
     }
     i = i + 1;
+}
+System.Console.WriteLine(max);
+
+max = array[0];
+for (int j = 0; j < n; j++)
+{
+    if (array[j] > max)
+    {
+        max = array[j];
+    }
+}
+System.Console.WriteLine(max);
+
+max = array[0];
+foreach(int e in array)
+{
+    if (e > max)
+    {
+        max = e;
+    }
 }
 System.Console.WriteLine(max);
