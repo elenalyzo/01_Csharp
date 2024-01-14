a5598eaf165b12dd9882b0a7a6e63d69844a094b
Elena Lyzo <elena.lyzo.69@gmail.com>
2024-01-14 16:28:03 +0300

    Задача

---
+++ Lesson2/Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿int n = 10;
+int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+int i = 0;
+
+while(i < n)
+{
+    if(array[i] % 2 == 0)
+    {
+        System.Console.Write($"{array[i]} ");
+    }
+    i = i + 1;
+}
