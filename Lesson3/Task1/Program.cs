b461f7224f7271d87f4c4c61e8df0d27f064492f
Elena Lyzo <elena.lyzo.69@gmail.com>
2024-01-15 21:53:17 +0300

    Задача 1

---
+++ Lesson3/Task1/Program.cs
@@ -0,0 +1,9 @@
+﻿double CalculateFormula(int a, int b, int c, int d)
+{
+    double numenator = a - b;
+    int denomenator = c + d;
+    double result = numenator / denomenator;
+    return result;
+}
+double result = CalculateFormula(15, 8, 3, 4);
+System.Console.WriteLine(result);
