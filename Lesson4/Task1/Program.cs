6ae1812eb51ddf44154e9aba28c37a95feeff783
Elena Lyzo <elena.lyzo.69@gmail.com>
2024-02-16 00:00:52 +0300

    Задача 1

---
+++ Lesson4/Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿int[,] malrix = new int[3, 5];
+Random rnd = new Random();
+for (int i = 0; i < malrix.GetLength(0); i++)
+{
+    for (int j = 0; j < malrix.GetLength(1); j++)
+    {
+        malrix[i, j] = rnd.Next(1, 11);
+    }
+}
+
+for (int i = 0; i < malrix.GetLength(1); i++)
+{
+    for (int j = 0; j < malrix.GetLength(0); j++)
+    {
+        Console.Write($"{malrix[i, j]} ");
+    }
+System.Console.WriteLine();
+}
